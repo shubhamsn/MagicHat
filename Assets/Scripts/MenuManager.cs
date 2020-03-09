d36d007cb233a5823f9421d51359783a5701839b
Shubham Soni <shubhamsn999@gmail.com>
2020-03-09 23:10:00 +0530

    Level menu added

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void StartGame()
+	{		
+		SceneManager.LoadScene ("LevelBG");
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+
+	public void OpneSettings()
+	{
+		
+	}
+}
